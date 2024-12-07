1b265e88498ee03669d7fbfb4c30b83c563aa36d
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-07 11:54:22 +0530

    structure

---
+++ Neura/neura/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      
+        <Header/>
+          <About/>
+
+          
+
+    
+        
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Neura/neura/src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center
+    contaner mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden
+    ' id='About'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2' >About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+        <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicate to your Vision</p>
+
+        <div className='flex flex-col md:flex-row items-center md:items-start  md:gap-20'>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default About
