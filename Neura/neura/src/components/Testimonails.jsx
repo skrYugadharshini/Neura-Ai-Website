07f632e831dbd0f7f9f39a83716cdc6344edea67
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-08 11:22:46 +0530

    about

---
+++ Neura/neura/src/components/Testimonails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonails = () => {
+  return (
+    <div>
+        <h1>Customer <span>Testimonials</span></h1>
+        <p>Real Stories from Those Who found home with us</p>
+    </div>
+  )
+}
+
+export default Testimonails
