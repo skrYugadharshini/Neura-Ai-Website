c068854fbd39ed9276b702477836d200957f12dc
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-08 16:12:20 +0530

    about

--- Neura/neura/src/components/Footer.jsx
+++ Neura/neura/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
             <div>
                 <img src={assets.logo_dark} alt="" />
                 <p>Lorem ipsum is the most well known filler text and comes from various passages of a book from Cicero, written in 45 BC.</p>
-                
+
             </div>
             <div></div>
             <div></div>
