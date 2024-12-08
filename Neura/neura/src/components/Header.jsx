1a05251ea439c65df19a45c0871228929d400533
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-08 17:56:38 +0530

    about

--- Neura/neura/src/components/Header.jsx
+++ Neura/neura/src/components/Header.jsx
@@ -13,7 +13,8 @@ const Header = () => {
       viewport={{once: true}}
 
       className='container text-center mx-suto py-4 px-6 md:px-20 lg:px-32 text-white'>
-        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
+
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams yu</h2>
         <div className='space-x-6 mt-16'>
           <a href="#Projects" className='border border-white px-8 py-3 rounded'>Projects</a>
           <a href="#Contact Us" className=' bg-blue-500 px-8 py-3 rounded'>Contact Us</a>
