59e8f335cc2896c7c04c2bc03ff1696d9e504b74
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-08 12:05:21 +0530

    about

--- Neura/neura/src/App.jsx
+++ Neura/neura/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
         <Header/>
           <About/>
           <Projects/>
+          <Testimonails/>
           
 
           
