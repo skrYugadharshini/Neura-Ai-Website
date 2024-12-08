4933224d55ccee242df6f715b5736d43e5439eaf
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-12-08 14:47:31 +0530

    about

--- Neura/neura/src/App.jsx
+++ Neura/neura/src/App.jsx
@@ -5,12 +5,15 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Testimonails from './components/Testimonails'
 import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       
         <Header/>
           <About/>
